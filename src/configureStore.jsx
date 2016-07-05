cf2b1b5bbd766fda6a1cf88735016a49ca663cc6
Zacaria <zchtatar@excilys.com>
2016-07-05 15:59:04 +0200

    Video 18, Applying the Middleware

--- src/configureStore.jsx
+++ src/configureStore.jsx
@@ -1,59 +1,17 @@
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import todoApp from './reducers';
-import throttle from 'lodash/throttle';
-
-const logger = (store) => (next) => {
-  if (!console.group) {
-    console.log('unsuppored');
-    return next;
-  }
-
-  return (action) => {
-    console.group(action.type);
-    console.log('%c prev state', 'color: gray', store.getState());
-    console.log('%c action', 'color: blue', action);
-    const returnValue = next(action);
-    console.log('%c next state', 'color:green', store.getState());
-    console.groupEnd(action.type);
-    return returnValue;
-  };
-};
-
-/*
-Mental model for ()=>()=>()=>
-
-A function with several arguments, applied as they become availiable
-*/
-
-const promise = (store) => (next) => (action) => {
-  if (typeof action.then === 'function') {
-    //delay action dispatch
-    return action.then(next);
-  }
-  return next(action);
-};
-
-const wrapDispatchWithMiddlewares = (store, middlewares) => {
-  middlewares
-    .slice()
-    .reverse()
-    .forEach(middleware => {
-      store.dispatch = middleware(store)(store.dispatch);
-    });
-};
+import promise from 'redux-promise';
+import createLogger from 'redux-logger';
 
 const configureStore = () => {
-  let store = createStore(todoApp);
-
+  // middlewares are applied in reverse order so that the promise is the upper
+  // wrapper of dispatch
   const middlewares = [promise];
-
   if (process.env.NODE_ENV !== 'production') {
-    middlewares.push(logger);
+    middlewares.push(createLogger());
   }
 
-  wrapDispatchWithMiddlewares(store, middlewares);
-
-  return store;
+  return createStore(todoApp, applyMiddleware(...middlewares));
 };
 
 export default configureStore;
