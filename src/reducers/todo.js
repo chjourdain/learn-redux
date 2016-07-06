6dc385a1d0f8c7bd974ab7c81ec0be2deff88010
chjourdain <cjourdain@excilys.com>
2016-07-06 11:47:22 +0200

    [11] Normalizing the State Shape

---
+++ src/reducers/todo.js
@@ -0,0 +1,26 @@
+/**
+ * Created by chjourdain on 06/07/16.
+ */
+
+const todo = (state, action) => {
+    switch (action.type) {
+        case 'ADD_TODO':
+            return {
+                id: action.id,
+                text: action.text,
+                completed: false
+            };
+        case 'TOGGLE_TODO':
+            if (state.id !== action.id) {
+                return state
+            }
+
+            return Object.assign({}, state, {
+                completed: !state.completed
+            });
+
+        default:
+            return state
+    }
+};
+export default todo;
